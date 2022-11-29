78c3fd4d41e03973a6e97be5be4f62763efe3bb2
Hilal Oruç <hiilaloruc@gmail.com>
2022-11-29 19:49:04 +0300

    Creation of the app

---
+++ App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello Worldd!</Text>
+    </SafeAreaView>
+  );
+};
+export default App;
