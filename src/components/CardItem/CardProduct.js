9ec54a2f898622fbc67d599cd957990a2c4d06f2
Hilal Oruç <hiilaloruc@gmail.com>
2022-12-17 01:01:14 +0300

    Products feed design complated

--- src/components/CardItem/CardProduct.js
+++ src/components/CardItem/CardProduct.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {View, Text, Image} from 'react-native';
 import styles from './CardProduct.style';
+import Star from 'react-native-star-view';
 
 const CardProduct = ({product}) => {
   return (
@@ -8,10 +9,17 @@ const CardProduct = ({product}) => {
       <Image style={styles.image} source={{uri: product.image}} />
       <View style={styles.inner_container}>
         <Text style={styles.title}>{product.title}</Text>
-        <Text style={styles.price}>{product.price} $</Text>
+        <View style={styles.subInfo}>
+          <Text style={styles.noStock}>{product.category}</Text>
+          <View style={styles.ratingContainer}>
+            <Star score={product.rating.rate} style={styles.starStyle} />
+            <Text style={styles.ratingCount}>({product.rating.count})</Text>
+          </View>
+
+          <Text style={styles.price}>{product.price} $</Text>
+        </View>
       </View>
     </View>
   );
 };
-
 export default CardProduct;
